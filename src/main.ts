7c362ad39e12353a73fbc0fc0474b8f723be9f9b
Luphia Chang <luphia@mermer.cc>
2024-02-15 16:08:10 +0800

    db test

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClient } from '@prisma/client'
 
 async function bootstrap() {
+  const prisma = new PrismaClient();
+  const result = await prisma.account_versions.findMany({
+    where: {
+      id: {
+        lte: 10,
+        gte: 1
+      }
+    }
+  });
+  console.log(result);
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
