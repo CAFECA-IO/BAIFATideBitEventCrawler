2e5ea5a739e886b1ea1a386d67b8069adb92bca5
Luphia Chang <luphia@mermer.cc>
2024-02-16 15:44:31 +0800

    update test code

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PrismaClient } from '@prisma/client'
+import { Sequelize } from 'sequelize';
+import 'dotenv/config';
 
 async function bootstrap() {
-  const prisma = new PrismaClient();
-  const result = await prisma.account_versions.findMany({
-    where: {
-      id: {
-        lte: 10,
-        gte: 1
-      }
-    }
-  });
-  console.log(result);
+  console.log(process.env.SOURCE_DATABASE_URL);
+  console.log(process.env.WAREHOUSE_DATABASE_URL);
+
+  const sourceDB = new Sequelize(process.env.SOURCE_DATABASE_URL);
+  const warehouseDB = new Sequelize(process.env.WAREHOUSE_DATABASE_URL);
+
+  const [results, metadata] = await sourceDB.query("SELECT * FROM account_versions WHERE id >= 1 AND id <= 10;");
+  console.log(results);
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
