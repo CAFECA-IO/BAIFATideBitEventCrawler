15e413815151346620f71db14f0c35063a7dabdf
Luphia Chang <luphia@mermer.cc>
2024-02-17 12:20:35 +0800

    update db module

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ async function bootstrap() {
   console.log(process.env.SOURCE_DATABASE_URL);
   console.log(process.env.WAREHOUSE_DATABASE_URL);
 
-  const sourceDB = new Sequelize(process.env.SOURCE_DATABASE_URL);
+  //const sourceDB = new Sequelize(process.env.SOURCE_DATABASE_URL);
   const warehouseDB = new Sequelize(process.env.WAREHOUSE_DATABASE_URL);
 
-  const [results, metadata] = await sourceDB.query("SELECT * FROM account_versions WHERE id >= 1 AND id <= 10;");
-  console.log(results);
+  //const [results, metadata] = await sourceDB.query("SELECT * FROM account_versions WHERE id >= 1 AND id <= 10;");
+  console.log(warehouseDB);
 }
 bootstrap();
